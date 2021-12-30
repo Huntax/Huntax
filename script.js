db396a24f6a9cb00b3a1bb3d8d7deb2c7f4b9379
lifeofsoumya <mondalsoumya2001@gmail.com>
2021-12-30 20:27:39 +0530

    separating each task with Functions and calling via mainjs

--- script.js
+++ script.js
@@ -17,46 +17,55 @@ var isrc = ()=> {
 
 // Grabbing OS Info from Landed user
 
-const os = require('os');
 
+function osInfo(){
 
-const username = os.userInfo().username;
-const uid = os.userInfo().uid;
-const homedir = os.userInfo().homedir;
-const shell = os.userInfo().shell;
-const osType = os.type();
-const osArch = os.arch();
-const osversion =os.version();
-const modelinfo = os.cpus()[1].model;
-const speedinfo = os.cpus()[1].speed;
-var freemem = (os.freemem() /1024/1024/1024).toFixed(2);
-var freeram = (os.totalmem() /1024/1024/1024).toFixed(2);
+    const os = require('os');
+    
+    
+    const username = os.userInfo().username;
+    const uid = os.userInfo().uid;
+    const homedir = os.userInfo().homedir;
+    const shell = os.userInfo().shell;
+    const osType = os.type();
+    const osArch = os.arch();
+    const osversion =os.version();
+    const modelinfo = os.cpus()[1].model;
+    const speedinfo = os.cpus()[1].speed;
+    var freemem = (os.freemem() /1024/1024/1024).toFixed(2);
+    var freeRam = (os.totalmem() /1024/1024/1024).toFixed(2);
+    
+    
+    console.log(username);
+    console.log(osType);
+    console.log(osArch);
+    console.log(uid);
+    console.log(homedir);
+    console.log(shell);
+    console.log(osversion);
+    console.log(modelinfo);
+    console.log(speedinfo);
+    console.log(`Free Space: ${freemem} GB`);
+    console.log(`Free RAM: ${freeRam} GB`)
+}
+osInfo()   
 
-
-        console.log(username);
-        console.log(osType);
-        console.log(osArch);
-        console.log(uid);
-        console.log(homedir);
-        console.log(shell);
-        console.log(osversion);
-        console.log(modelinfo);
-        console.log(speedinfo);
-        console.log(`Free Space: ${freemem} GB`);
-        console.log(`Free RAM: ${freeram} GB`)
 
 
 // Getting IP Address of the Landed User
 
-fetch('https://api.ipify.org/?format=json')
-        .then(results => results.json()).then(data => ipAdr= data.ip );
+function getLocation(){
 
-fetch('https://geolocation-db.com/json/')
-        .then(results => results.json()).then(data => console.log(`Latitude: ${data.latitude}, Longitude: ${data.longitude}`));
-
+    fetch('https://api.ipify.org/?format=json')
+    .then(results => results.json()).then(data => ipAdr= data.ip );
+    
+    fetch('https://geolocation-db.com/json/')
+    .then(results => results.json()).then(data => console.log(`Latitude: ${data.latitude}, Longitude: ${data.longitude}`));
+}
+getLocation()
 
 // Detecting the user's browser
-
+function browserFind(){
 var browser = '';
 
 var isOpera = (window.opr && opr.addons) || window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;{
@@ -79,8 +88,8 @@ var isChrome = window.chrome && (window.chrome.webstore || window.chrome.runtime
 
 var isBlink = (isChrome || isOpera) && window.CSS;{
     browser = 'Blink';}
-
-
+}
+browserFind()
 
 // Creating a json file from the received data
 
@@ -88,7 +97,6 @@ var isBlink = (isChrome || isOpera) && window.CSS;{
 var array = {};
 var elements = []
 array.elements = elements;
-console.log(array);
 
 var singleElement = {
     "OS Name": osType,
