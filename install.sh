f03715f0fa61af41f5ff931b9d5b6c13f285b4af
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-01 19:08:15 +0530

    install python ssh requests with shell

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 echo '[!] Updating...'
-sudo apt update
+sudo apt-get update
+echo
 echo '[!] Installing Dependencies...'
 echo '    JS'
 sudo apt install nodejs npm
+echo '    Python3'
+apt-get -y install python3 python3-pip
+echo '    ssh'
+apt-get -y install ssh
+echo '    Requests'
+pip3 install requests
+echo
 echo '[!] Setting Permissions...'
 chmod 777 Tracker/result.txt
 echo
