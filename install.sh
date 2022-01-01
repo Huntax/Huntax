3fd0312aecc973307a12fb9710bd02bcb3ffe9f3
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-01 12:07:11 +0530

    shell files changed

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo '[!] Updating...'
+echo '[!] Installing Dependencies...'
+echo '    Python3'
+
+chmod 777 Tracker/
+
---
+++ results.sh
@@ -0,0 +1,3 @@
+echo "Sucessfully received Info of Target.."
+echo "Fetching information of Target..."
+echo result.txt
