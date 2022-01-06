f98d185a4441f78f450e9ae9fad04f72c3062ca0
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-06 14:52:36 +0530

    added installing php function sh

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@ echo '[!] Updating...'
 sudo apt-get update
 echo
 echo '[!] Installing Dependencies...'
-echo '    JS'
-sudo apt install nodejs npm
 echo '    Python3'
 apt-get -y install python3 python3-pip
 echo '    ssh'
 apt-get -y install ssh
+echo '    PHP'
+apt-get -y install php
 echo '    Requests'
 pip3 install requests
+echo '    JS'
+sudo apt install nodejs npm
 echo
 echo '[!] Setting Permissions...'
 chmod 777 Tracker/result.txt
