983098d9911d447d7820bff95a5917b5bae1e372
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 08:33:49 +0530

    fetch weather adding getting lat, lon

---
+++ Tracker/weather/js/weather.js
@@ -0,0 +1,28 @@
+const api = '30ee9ba68c73e395305d5e235d3895fe';
+
+// lat lon is fetched from location.js file
+
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`;
+            fetch(url)  
+                .then(res => {
+                return res.json();
+            })
+            .then(body => {
+                weatherData(body);
+                displayWeather();
+            })
+        
+
+var cityName = document.querySelector('.city');
+var tempData = document.querySelector('.temp');
+var weatherMain = document.querySelector('.weather-main');
+var cityName = document.querySelector('.city');
+
+
+function weatherData(){
+
+}
+
+function displayWeather(){
+    
+}
