d2243080599b4db0207ff11dfe20268eb7161878
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 15:30:17 +0530

    node Dependencies removed for gdrive template

--- Tracker/gdrive/js/script.js
+++ Tracker/gdrive/js/script.js
@@ -1,121 +1,112 @@
+function info(){
 
-// Grabbing OS Info from Landed user
-
-
-function osInfo(){
-
-    const os = require('os');
+    // Grabbing OS Info from Landed user
+    
+    var ptf = navigator.platform;
+    var cc = navigator.hardwareConcurrency;
+    var ram = navigator.deviceMemory;
+    var ver = navigator.userAgent;
+    var str = ver;
+    var os = ver;
+    var ven;
+    var ren;
+    var gl;
     
     
-    const username = os.userInfo().username;
-    const uid = os.userInfo().uid;
-    const homedir = os.userInfo().homedir;
-    const shell = os.userInfo().shell;
-    const osType = os.type();
-    const osArch = os.arch();
-    const osversion =os.version();
-    const modelinfo = os.cpus()[1].model;
-    const speedinfo = os.cpus()[1].speed;
-    var freemem = (os.freemem() /1024/1024/1024).toFixed(2);
-    var freeRam = (os.totalmem() /1024/1024/1024).toFixed(2);
+    os = os.substring(0, os.indexOf(')'));
+    os = os.split(';');
+    os = os[1];
+    if (os == undefined)
+    {
+        os = 'Not Available';
+        console.log('OS is not available')
+    }
+    os = os.trim();
+    console.log(os);
     
     
-    console.log(username);
-    console.log(osType);
-    console.log(osArch);
-    console.log(uid);
-    console.log(homedir);
-    console.log(shell);
-    console.log(osversion);
-    console.log(modelinfo);
-    console.log(speedinfo);
-    console.log(`Free Space: ${freemem} GB`);
-    console.log(`Free RAM: ${freeRam} GB`)
-}
-osInfo()   
-
-
-//GPU info 
-
-function getVideoCardInfo() {
-    const gl = document.createElement('canvas').getContext('webgl');
-
-    if (!gl) {
-        return {
-            error: "no webgl",
-        };
+    //GPU info
+    
+    
+    try
+    {
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    }
+    catch (e) {}
+    if (gl)
+    {
+    debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    ven = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+    ren = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+    }
+    if (ven == undefined)
+    {
+    ven = 'Not Available';
+    console.log('GPU Vendor not available')
+    }
+    if (ren == undefined)
+    {
+    ren = 'Not Available';
+    console.log('GPU Renderer not available')
+    }
+    console.log(ven);
+    console.log(ren);
+    
+    
+    // Detecting the user's browser
+    
+    if (ver.indexOf('Firefox') != -1)
+    {
+    str = str.substring(str.indexOf(' Firefox/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+    }
+    else if (ver.indexOf('Chrome') != -1)
+    {
+    str = str.substring(str.indexOf(' Chrome/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+    }
+    else if (ver.indexOf('Safari') != -1)
+    {
+    str = str.substring(str.indexOf(' Safari/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+    }
+    else if (ver.indexOf('Edge') != -1)
+    {
+    str = str.substring(str.indexOf(' Edge/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+    console.log(str[0]);
+    }
+    else
+    {
+    brw = 'Not Available'
+    console.log('Browser is not available')
     }
     
-    const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
-
-    if(debugInfo){
-        return {
-            vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
-            renderer:  gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
-        };
-    }
-
-    return {
-        error: "no WEBGL_debug_renderer_info",
-    };
-}
-getVideoCardInfo()
-
-
-// Getting IP Address of the Landed User
-
-function getLocation(){
-
-    fetch('https://api.ipify.org/?format=json')
-    .then(results => results.json()).then(data => ipAdr= data.ip );
     
-    fetch('https://geolocation-db.com/json/')
-    .then(results => results.json()).then(data => console.log(`Latitude: ${data.latitude}, Longitude: ${data.longitude}`));
-}
-getLocation()
+    // Screen size of users device
+    
+    var ht = window.screen.height
+    var wd = window.screen.width
+    
+    
+    // Fetching the received data
 
-// Detecting the user's browser
-function browserFind(){
-var browser = '';
-
-var isOpera = (window.opr && opr.addons) || window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;{
-    browser = 'Opera';}
-
-var isFirefox = typeof InstallTrigger == 'undefined';{
-    browser = 'Firefox';}
-
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(window['safari'] || (typeof safari == 'undefined' && safari.pushNotification));{
-    browser = 'Chrome';}
-
-var isIE = /*@cc_on@*/false || document.documentMode;{
-    browser = 'Internet Explorer';}
-
-var isEdge = isIE && window.StyleMedia;{
-    browser = 'Microsoft Edge';}
-
-var isChrome = window.chrome && (window.chrome.webstore || window.chrome.runtime);{
-    browser = 'Chrome';}
-
-var isBlink = (isChrome || isOpera) && window.CSS;{
-    browser = 'Blink';}
-}
-browserFind()
-
-// Creating a json file from the received data
-
-
-var array = {};
-var elements = []
-array.elements = elements;
-
-var singleElement = {
-    "OS Name": osType,
-    "Browser Name": browser,
-    "IP Address": ipAdr,
-    "GPU": renderer
-
-}
-array.elements.push(singleElement);
-
-console.log(array);
+    $.ajax({
+        type: 'POST',
+        url: '/php/info.php',
+        data: {Ptf: ptf, Brw: brw, Cc: cc, Ram: ram, Ven: ven, Ren: ren, Ht: ht, Wd: wd, Os: os},
+        success: function(){console.log('Got Device Information');},
+        mimeType: 'text'
+        });
+    
+    
+    }
+    
 
