eab9613b7a30cc0a8d6d443cf216ea17dd6360dc
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 15:14:54 +0530

    results improved with weather results

--- results.sh
+++ results.sh
@@ -2,3 +2,4 @@ echo "Sucessfully received Info of Target.."
 echo "Fetching information of Target..."
 echo /Tracker/filedownload/result.txt
 echo /Tracker/gdrive/result.txt
+echo /Tracker/weather/result.txt
