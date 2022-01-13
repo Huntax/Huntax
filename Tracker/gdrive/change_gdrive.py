8195d182af027ae622783d99422b663c87c137d1
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 15:29:07 +0530

    python removed for redirect url replacing, used shell directly

--- Tracker/gdrive/change_gdrive.py
+++
@@ -1,7 +0,0 @@
-redirect = input(G + '[+]' + C + ' Enter GDrive File URL : ' + W)
-with open('/js/temploc.js', 'r') as js:
-	reader = js.read()
-	update = reader.replace('REDIRECT_URL', redirect)
-
-with open('/js/script.js', 'w') as js_update:
-	js_update.write(update)
